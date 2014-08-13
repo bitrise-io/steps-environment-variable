f187a3161679fa739223f1739ba8aa5bbb7cbe72
erosdome <erosdome@gmail.com>
2014-08-13 17:48:26 +0200

    formatted output update

--- step.sh
+++ step.sh
@@ -14,5 +14,18 @@ function write_section_to_formatted_output {
 
 echo "Environment Key: $ENVIRONMENT_KEY"
 echo "Environment Value: $ENVIRONMENT_VALUE"
+
+if [ ! -n "$ENVIRONMENT_KEY" ]; then
+  echo " [!] ENVIRONMENT_KEY is missing! Terminating..."
+  echo
+  write_section_to_formatted_output "# Error!"
+  write_section_to_formatted_output "Reason: enviroment key is missing."
+  exit 1
+fi
+
 echo "export $ENVIRONMENT_KEY=\"$ENVIRONMENT_VALUE\"" >> ~/.bash_profile
-write_section_to_formatted_output "**${ENVIRONMENT_KEY}** = ${ENVIRONMENT_VALUE}"
+if [ ! -n "$ENVIRONMENT_VALUE" ]; then
+	write_section_to_formatted_output "**${ENVIRONMENT_KEY}** = **not provided (empty)**"
+else
+	write_section_to_formatted_output "**${ENVIRONMENT_KEY}** = ${ENVIRONMENT_VALUE}"
+fi
