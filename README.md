32df5bafe9b6797b778972c54b357a3c8f8b1736
erosdome <erosdome@gmail.com>
2014-08-13 11:51:53 +0200

    initial commit

--- README.md
+++ README.md
@@ -4,8 +4,12 @@ steps-environment-variable
 Define an Environment Variable for the upcoming Steps
 
 # Input Environment Variables
-- ENVIRONMENT_KEY
-- ENVIRONMENT_VALUE
+- **ENVIRONMENT_KEY**
+
+	the key of the enviroment variable; should not countain the leading $ sign
+- **ENVIRONMENT_VALUE**
+
+	the value of the enviroment variable
 
 # Output
 Will add (append) the defined key=value to the user's ~/.bash_profile
--- step.sh
+++ step.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+formatted_output_file_path="$BITRISE_STEP_FORMATTED_OUTPUT_FILE_PATH"
+
+function echo_string_to_formatted_output {
+  echo "$1" >> $formatted_output_file_path
+}
+
+function write_section_to_formatted_output {
+  echo '' >> $formatted_output_file_path
+  echo "$1" >> $formatted_output_file_path
+  echo '' >> $formatted_output_file_path
+}
+
 echo "Environment Key: $ENVIRONMENT_KEY"
 echo "Environment Value: $ENVIRONMENT_VALUE"
-
 echo "export $ENVIRONMENT_KEY=\"$ENVIRONMENT_VALUE\"" >> ~/.bash_profile
+write_section_to_formatted_output "**${ENVIRONMENT_KEY}** = ${ENVIRONMENT_VALUE}"
